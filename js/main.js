beda56582131b1716897d18ad272925dfc513cbf
hayeun7777 <kellyhypark@gmail.com>
2019-01-04 15:16:34 -0800

    small change

---
+++ js/main.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){
+document.getElementById("mainBtn").addEventListener("click", diamond);
+
+})
+function diamond(){
+	console.log("it's working");
+};
